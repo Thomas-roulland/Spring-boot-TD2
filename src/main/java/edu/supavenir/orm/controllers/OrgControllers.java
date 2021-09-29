178db0369ef03bef80dc3dc4f542f8533196dd4c
Thomas Roulland <troulland@sio.local>
2021-09-29 11:46:03 +0200

    up

--- src/main/java/edu/supavenir/orm/controllers/OrgControllers.java
+++ src/main/java/edu/supavenir/orm/controllers/OrgControllers.java
@@ -6,16 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 import edu.supavenir.orm.models.Groupe;
 import edu.supavenir.orm.models.Organization;
 import edu.supavenir.orm.repositories.OrgRepositories;
+import edu.supavenir.orm.tricks.CssMessage;
 
 @Controller
-@RequestMapping("/org/")
+@RequestMapping("org")
 public class OrgControllers {
 
     @Autowired
@@ -42,4 +46,34 @@ public class OrgControllers {
 	repository.saveAndFlush(orga);
 	return "orga ajouté : " + orga;
     }
+
+    @GetMapping("edit/{id}")
+    public String edit(@PathVariable int id, Model model) {
+	Organization bdOrga = repository.getById(id);
+	model.addAttribute("organizations", bdOrga);
+	return "editOrg";
+    }
+
+    @PostMapping("edit/{id}")
+    public RedirectView edit(@PathVariable int id, Organization orga) {
+	Organization bdOrga = repository.getById(id);
+	bdOrga.setName(orga.getName());
+	bdOrga.setDomain(orga.getDomain());
+	bdOrga.setAliases(orga.getAliases());
+	repository.saveAndFlush(bdOrga);
+	return new RedirectView("/org/");
+    }
+
+    @GetMapping("delete/{id}")
+    public RedirectView delete(@PathVariable int id, Model model, RedirectAttributes attrs) {
+	Organization org = repository.getById(id);
+	model.addAttribute("organizations", org);
+	CssMessage Suppresion = new CssMessage("SuccesFull", "error", "exclamation circle");
+	attrs.addFlashAttribute("msg", Suppresion);
+	repository.deleteById(id);
+	return new RedirectView("/org/");
+    }
+    
+    @GetMapping("display")
+
 }
---
+++ tricks/CssMessage.java
@@ -0,0 +1,54 @@
+package edu.supavenir.tricks;
+
+public class CssMessage {
+	private String content;
+	private String type;
+	private String icon;
+	
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public String getIcon() {
+		return icon;
+	}
+	
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+	
+	public CssMessage(String content, String type, String icon) {
+	
+		this.content = content;
+		this.type = type;
+		this.icon = icon;
+	}
+	
+	
+	public static CssMessage SuccesMessage(String content) {
+		return new CssMessage(content, "success", "check");
+	}
+	
+	public static CssMessage ErrorMessage(String content) {
+		return new CssMessage(content, "error", "exclamation circle");
+	}
+	@Override
+	public String toString() {
+		return "CssMessage [content=" + content + ", type=" + type + ", icon=" + icon + "]";
+	}
+	
+	
+	
+	
+}
